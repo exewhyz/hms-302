33716bb97c427cb71371eaeac06aad0126d71385
Aniket Raj <araj52444@gmail.com>
2025-06-19 10:52:04 +0000

    updated entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,26 @@
-import React from 'react'
-import Table from './components/Table'
+import Login from "./pages/Login.jsx"
+import Register from "./pages/Register.jsx"
+import Booking from "./pages/Booking.jsx"
+import Home from "./pages/Home.jsx"
+import Prescriptions from "./pages/Prescriptions.jsx"
+import Navbar from './components/Navbar.jsx'
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-
-  const appointments = [
-    {
-      id: 1,
-      doctor: "Dr. Aditya",
-      date: "2025-06-16",
-      time: "10:56:10"
-    },
-    {
-      id: 2,
-      doctor: "Dr. Tarun",
-      date: "2025-06-26",
-      time: "10:56:10"
-    },
-    {
-      id: 3,
-      doctor: "Dr. Rohit",
-      date: "2025-06-26",
-      time: "11:58:10"
-    }
-  ]
-
-  const upcommingAppointments = appointments.filter((app) => new Date(app.date) > Date.now())
-  const pastAppointments = appointments.filter((app) => new Date(app.date) < Date.now())
   return (
-    <div className='flex flex-col gap-8'>
-      <div>
-        <h1 className='font-bold text-4xl'>Upcomming Appointments</h1>
-        <div className='flex gap-4 p-6'>
-          {
-            upcommingAppointments.map((app) => {
-              return (
-                <Table key={app.id} id={app.id} doctor={app.doctor} date={app.date} time={app.time} />
-              )
-            })
-          }
-        </div>
-      </div>
-      <div>
-        <h1 className='font-bold text-4xl'>Past Appointments</h1>
-        <div className='flex gap-4 p-6'>
-          {
-            pastAppointments.map((app) => {
-              return (
-                <Table key={app.id} id={app.id} doctor={app.doctor} date={app.date} time={app.time} />
-              )
-            })
-          }
-        </div>
-      </div>
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className='h-screen px-8 pt-16 pb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/booking' element={<Booking />} />
+          <Route path='/prescriptions' element={<Prescriptions />} />
+        </Routes>
+      </main>
+    </>
   )
 }
