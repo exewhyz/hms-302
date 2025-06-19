91c1f8111f9bb8dbe6b4af4c175695062857b9c4
Aniket Raj <araj52444@gmail.com>
2025-06-19 10:58:06 +0000

    protected route implmented

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Booking from "./pages/Booking.jsx"
 import Home from "./pages/Home.jsx"
 import Prescriptions from "./pages/Prescriptions.jsx"
 import Navbar from './components/Navbar.jsx'
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useAuth } from "./context/AuthContext.jsx"
 
 export default function App() {
+  const { isAuthenticated } = useAuth();
   return (
     <>
       <header>
@@ -14,11 +16,13 @@ export default function App() {
       </header>
       <main className='h-screen px-8 pt-16 pb-4'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          {/* public routes */}
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/booking' element={<Booking />} />
-          <Route path='/prescriptions' element={<Prescriptions />} />
+          {/* protected routes */}
+          <Route path='/' element={isAuthenticated ? <Home /> : <Navigate to={"/login"} />} />
+          <Route path='/booking' element={isAuthenticated ? <Booking /> : <Navigate to={"/login"} />} />
+          <Route path='/prescriptions' element={isAuthenticated ? <Prescriptions /> : <Navigate to={"/login"} />} />
         </Routes>
       </main>
     </>
